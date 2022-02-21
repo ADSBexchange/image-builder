470ce46ac12b014e44b81182c40c040f08cc66b0
Matthias Wirth <matthias.wirth@gmail.com>
2022-02-21 14:11:13 +0000

    use 127.0.0.1 instead of localhost

--- skeleton/usr/local/bin/adsbexchange-feed.sh
+++ skeleton/usr/local/bin/adsbexchange-feed.sh
@@ -9,7 +9,7 @@ do
 			--db-file=none --max-range 450 \
                         --net-beast-reduce-interval 0.5 \
 			--net-connector feed.adsbexchange.com,30004,beast_reduce_out,feed.adsbexchange.com,64004 \
-                        --net-connector localhost,30005,beast_in \
+                        --net-connector 127.0.0.1,30005,beast_in \
                         --net-ro-interval 0.2 --net-ri-port 0 --net-ro-port 0 \
                         --net-sbs-port 0 --net-bi-port 0 --net-bo-port 0 \
                         --json-location-accuracy 2 --write-json /run/adsbexchange-feed \
